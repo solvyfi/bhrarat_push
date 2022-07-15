263d0c96dffca3964ba33ed486343d7a6567332b
solvyfi <n30@solvyfi.com>
2022-07-15 15:31:10 +0530

    max to req_part

--- models/question.py
+++ models/question.py
@@ -8,28 +8,21 @@ from typing import Optional, Any
 from beanie import PydanticObjectId
 
 
-
-
 class Question(Document):
     question : str
     option : list
     context : Optional[str]
     target_responder : list
-    max_part : int
+    req_part : int
     curr_part : Optional[int]
     bounty : float = 0.0 
-    #option1 : str
-    #option2 : str
-    #option3 : str
-    #option4 : str
-    #option5 : str#2222s
-    #option6 : str
     time_asked : str
     time_answered : Optional[str]
     likes : Optional[list]
     comment : Optional[dict]
     comment_text : Optional[str]
-    commenter : Optional[str]#PydanticObjectId]
+    commenter : Optional[str]
+    vote : Optional[int]
     share : Optional[list]
     category : Optional[list]
     tags : Optional[list]
@@ -43,23 +36,18 @@ class Question(Document):
             "example" : {
                 "question" : "Type your question",
                 "context" : "text",
-                "target_responder" : [],
+                "target_responder" : [], # Public , GroupID or IndividualIDs
                 "option" : [],
-                "max_part" : "10",
+                "req_part" : "10",
                 "curr_part" : "1",
                 "bounty" : "9.99",
                 "time_asked" : "time_asked",
                 "time_answered" : "time_answered",
-                #"option 1" : "option 1",
-                #"option 2" : "option 1",
-                #option 3" : "option 1",
-                #"option 5" : "option 1",
-                #"option 5" : "option 1",
-                #"option 6" : "option 1",
                 "like" : [],
                 "comment" : {
                     "commenter" : "john Doe",
                     "comment_text" : "That's a great comment! \m/",
+                    "vote" : "0"
                 },
                 "share" : [],
                 "category" : [],
